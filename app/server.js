f929f1138e21829daaeb7a619a964511d6c6cbc6
Alberto <alberto@mendezcabrera.com>
2017-10-01 21:14:20 +0200

    - Corregido un error que hacía que el volumen no  persistiera los datos por tema de permisos - Añadidas nuevas dependencias a npm

--- app/server.js
+++ app/server.js
@@ -1,3 +1,8 @@
+'use strict'
+
+const pg = require('pg')
+const conString = 'postgres://postgres:@pgdb/node_tutorial_db'
+
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
@@ -8,6 +13,20 @@ const HOST = '0.0.0.0'
 
 // App
 const app = express()
+
+/*pg.connect(conString, function(err, client, done){
+    if(err){
+        return console.error('error fetching client from pool', err)
+    }
+    client.query('SELECT $1::varchar AS my_first_query', ['node_tutorial_db'], function(err, result){
+        done()
+        if(err){
+            return console.error('error happened during query', err)
+        }
+        console.log(result.rows[0])
+        process.exit(0)
+    })
+})  */
 
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
